073244c8fcf8e2ae1340765340cbf127892d795e
Eduard Nitu <edi.nitu2000@yahoo.com>
2025-10-17 11:17:56 +0200

    search: workaround for version search bug

--- invenio_records_global_search/services/utils.py
+++ invenio_records_global_search/services/utils.py
@@ -9,6 +9,8 @@
 
 import re
 
+from invenio_records_resources.services.records.results import RecordList
+
 
 def replace_query_params_with_known_schema(full_q: str, known_field_name: str) -> str:
     """Replace query fields.
@@ -21,5 +23,30 @@ def replace_query_params_with_known_schema(full_q: str, known_field_name: str) -
     matches = re.findall(pattern, full_q)
     for match in matches:
         result_q = result_q.replace(match, f"metadata.{known_field_name}")
+
+    return result_q
+
+
+def build_gs_id_query(original_records: RecordList) -> str:
+    """Build global-search id query.
+
+    Based on the original records, translate the original record IDs
+    to Global Search IDs.
+    """
+    result = ""
+    for record in original_records.hits:
+        result += f"id: gs-{record.get("id")} "
+
+    return result.strip()
+
+
+def remove_parent_id_q(query: str) -> str:
+    """Remove all appearances of parent.id:<example_id> from given query."""
+    pattern = re.compile(r"\bparent\.id:\s?\S+")
+    matches = re.findall(pattern, query)
+
+    result_q = query
+    for match in matches:
+        result_q = result_q.replace(match, "")
 
     return result_q
