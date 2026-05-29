c7775469383eb904550d83e3c59e859dd66ed3cc
Eduard Nitu <edi.nitu2000@yahoo.com>
2026-05-29 12:35:24 +0200

    feat(cli): update database CLI functions
      * add new update_missing CLI functions which adds records
        that are not yet in the gs database. can specify to run
        only for one data model
      * update rebuild_database CLI function with data-model option

--- invenio_records_global_search/cli.py
+++ invenio_records_global_search/cli.py
@@ -16,6 +16,12 @@ from invenio_access.permissions import system_identity
 from .proxies import current_records_global_search
 
 
+def _extract_datamodel_from_gs_config(cfg: str) -> list[str]:
+    funcs = current_app.config.get(cfg, [])
+    func_base_name = cfg.removeprefix("GLOBAL_SEARCH_").lower()
+    return [func.__name__.removeprefix(func_base_name) for func in funcs]
+
+
 @click.group()
 def global_search() -> None:
     """CLI-group for "invenio record global search" commands."""
@@ -34,8 +40,42 @@ def rebuild_index() -> None:
 
 
 @global_search.command()
+@click.option(
+    "--model",
+    default=None,
+    help="Run only the rebuild function for this data model.",
+)
 @with_appcontext
-def rebuild_database() -> None:
+def rebuild_database(model: str | None) -> None:
     """Rebuild database."""
-    for func in current_app.config.get("GLOBAL_SEARCH_REBUILD_DATABASE", []):
+    funcs = current_app.config.get("GLOBAL_SEARCH_REBUILD_DATABASE", [])
+    if model:
+        funcs = [func for func in funcs if model in func.__name__]
+
+        if not funcs:
+            msg = f"No rebuild function for data model '{model}'. Known options: {_extract_datamodel_from_gs_config("GLOBAL_SEARCH_REBUILD_DATABASE")}"
+            raise click.ClickException(msg)
+
+    for func in funcs:
+        func()
+
+
+@global_search.command()
+@click.option(
+    "--model",
+    default=None,
+    help="Update only missing GS Records for this data model.",
+)
+@with_appcontext
+def update_missing(model: str | None) -> None:
+    """Update GS database with missing records from original data-models."""
+    funcs = current_app.config.get("GLOBAL_SEARCH_UPDATE_MISSING", [])
+    if model:
+        funcs = [func for func in funcs if model in func.__name__]
+
+        if not funcs:
+            msg = f"No update missing function for data model '{model}'. Known options: {_extract_datamodel_from_gs_config("GLOBAL_SEARCH_UPDATE_MISSING")}"
+            raise click.ClickException(msg)
+
+    for func in funcs:
         func()
