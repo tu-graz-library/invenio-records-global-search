a9e0db5b98550c5ad7dddcb383a5c623630bb3f9
Christoph Ladurner <christoph.ladurner@tugraz.at>
2026-06-01 16:05:53 +0200

    feat(search): add GLOBAL_SEARCH_NAV_TEMPLATE config
    * config: add GLOBAL_SEARCH_NAV_TEMPLATE variable
      to allow downstream apps to inject a custom nav
      template above the search app.

--- invenio_records_global_search/config.py
+++ invenio_records_global_search/config.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2023-2025 Graz University of Technology.
+# Copyright (C) 2023-2026 Graz University of Technology.
 #
 # invenio-records-global-search is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -78,6 +78,13 @@ GLOBAL_SEARCH_SEARCH = {
 }
 
 GLOBAL_SEARCH_BASE_TEMPLATE = "invenio_records_global_search/base.html"
+
+GLOBAL_SEARCH_NAV_TEMPLATE = None
+"""Template injected above the global search app.
+
+Set to a template path to render a custom nav above the search results
+e.g. a resource type switcher. ``None`` disables the injection.
+"""
 
 GLOBAL_SEARCH_ORIGINAL_SCHEMAS: dict[str, dict[str, str]] = {}
 """Configure the original schema names."""
